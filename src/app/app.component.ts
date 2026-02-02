4c28b3e9c3e87647343788b7dcace615533f6ccc
unknown <piyush.jogi@neosoftmail.com>
2026-02-02 19:10:39 +0530

    explore directives with list rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,42 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'pj-angular-app';
   username = 'piyush';
   count = 7;
   name = '';
+  login = false
+  isActive = true
+  isError = false
+
+  courseList = ["DBMS","java","angular","spring boot","docker","K8","Postgres"];
+
+  courseInfo = [
+    {
+      name : 'piyush',
+      age : 24,
+      email : 'piyush.jogi@neosoftmail.com'
+    },
+    {
+      name : 'puneet',
+      age : 25,
+      email : 'puneet.arya@neosoftmail.com'
+    },
+    {
+      name : 'manav',
+      age : 28,
+      email : 'manav.singh@neosoftmail.com'
+    }
+  ]
 
   isDisabled = true;
-
+  display = false;
   increment() {
     this.count++;
   }
@@ -24,6 +48,5 @@ export class AppComponent {
   reset(){
     this.count = 0;
   }
-
 
 }
